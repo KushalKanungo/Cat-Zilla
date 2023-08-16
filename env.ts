445b7d9c508aac465608b8ce60885f3b19f2d705
Kushal Kanungo <kushalkhandelwal27@gmail.com>
2023-08-17 01:00:55 +0530

    changed backend url

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 const ENV = {
   // BASE_URL: 'http://10.42.0.204:3000/api'
-  BASE_URL: 'http://localhost:3200/api',
-  // BASE_URL: 'https://cat-zilla-backend.onrender.com/api',
+  // BASE_URL: 'http://localhost:3200/api',
+  BASE_URL: 'https://cat-zilla-backend.onrender.com/api',
   ACCESS_TOKEN_KEY_NAME: 'accessToken'
 }
 Object.freeze(ENV)
